e5acb2c34cd9ac4be5f21d917a87d4a881468afb
wendongchao <dongchao1860369@sina.com>
2021-09-17 19:15:09 +0800

    同步代码块

---
+++ thread-02-synchronize/src/main/java/com/thread/synchronize/SynchronizedThread12.java
@@ -0,0 +1,17 @@
+package com.thread.synchronize;
+
+
+/**
+ * synchronized同步语句块
+ * 一半同步，一半异步
+ * 不在synchronized块中异步执行，在synchronized块中同步执行。
+ * 示例：
+ * @Author wendongchao
+ * @Date 2021/9/11 17:55
+ */
+public class SynchronizedThread12 {
+
+    public static void main(String[] args) {
+
+    }
+}
